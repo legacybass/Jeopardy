4788ac768f427e72c9f654a773ed99e69cd48892
Daniel Beus <legacybass.developer@outlook.com>
2014-11-07 17:25:06 -0700

    Removed compiled JS files
    Removed compiled JS files from the public folder.

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-define([], function () {
-    "use strict";
-    document.getElementById('nojavascript').style.display = 'none';
-    require(['router'], function (router) {
-        router.SetupRoutes('#main');
-        router.Start();
-    });
-    return {};
-});
